8d1d67c250718d0b08313eb29f9f0ccea346ac00
Hector Bobbio <hbobbioh@uni.pe>
2023-11-13 12:09:49 -0500

    OmniAuth gem and config added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+# Replace API_KEY and API_SECRET with the values you got from Twitter
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :developer if Rails.env.development?
+    provider :twitter, "API_KEY", "API_SECRET"
   end
