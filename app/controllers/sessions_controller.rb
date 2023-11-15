36532bdbefdd981a0c6de76fee2665a9bf32e25f
Hector Bobbio <hbobbioh@uni.pe>
2023-11-15 00:37:27 -0500

    Fixed Google Auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,19 @@
 class SessionsController < ApplicationController
-    def create
-        auth = request.env['omniauth.auth']
-        session[:user_token] = auth.credentials.token
-        session[:user_email] = auth.info.email
-        redirect_to root_path
-      end
-    
-      def destroy
-        reset_session
-        redirect_to root_path
-      end
+  # login & logout actions should not require user to be logged in
+  skip_before_action :set_current_user  # check you version
+  def create
+    begin
+      @user = Moviegoer.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id
+    rescue => error
+      flash[:warning] = error.message
+    end
+    redirect_to root_path
+  end
+  
+  def destroy
+    reset_session
+    flash[:notice] = 'Logged out successfully.'
+    redirect_to movies_path
+  end
 end
--- app/models/moviegoer.rb
+++ app/models/moviegoer.rb
@@ -1,10 +1,11 @@
 # Edit app/models/moviegoer.rb to look like this:
 class Moviegoer < ActiveRecord::Base
-    has_many :reviews
-    def self.create_with_omniauth(auth)
-        Moviegoer.create!(
-        :provider => auth["provider"],
-        :uid => auth["uid"],
-        :name => auth["info"]["name"])
-    end
+    class << self
+        def from_omniauth(auth_hash)
+          user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+          user.name = auth_hash['info']['name']
+          user.save!
+          user
+        end
+      end
 end
